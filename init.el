dd508a6df1263d9d7faa66b956a7ebe5e7d688e3
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-09-14 16:23:03 +0200

    Custom set variables

--- init.el
+++ init.el
@@ -1,9 +1,13 @@
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
-     "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(send-mail-function (quote smtpmail-send-it)))
 
 (package-initialize)
 (setq package-archives
