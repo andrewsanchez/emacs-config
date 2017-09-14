6bd68441b283890a892c72c841fb2545a3888d65
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-09-14 16:24:59 +0200

    create gnus.el

---
+++ gnus.el
@@ -0,0 +1,12 @@
+(setq user-mail-address "inbox.asanchez@gmail.com"
+      user-full-name "Andrew Sanchez")
+
+(setq gnus-select-method
+      '(nnimap "gmail"
+	       (nnimap-address "imap.gmail.com")  ; it could also be imap.googlemail.com if that's your server.
+	       (nnimap-server-port "993")
+	       (nnimap-stream ssl)))
+
+(setq smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
