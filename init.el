f9396c35be49d8b420dcdace899a36e894300dd3
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-10-29 13:43:24 +0100

    Move into gnus config

--- init.el
+++ init.el
@@ -72,7 +72,6 @@
 (add-to-list 'evil-emacs-state-modes 'dired-mode)
 (add-to-list 'evil-emacs-state-modes 'flycheck-error-list-mode)
 (add-to-list 'evil-emacs-state-modes 'elfeed-search-mode)
-(add-to-list 'evil-emacs-state-modes 'gnus-group-mode)
 (add-to-list 'evil-emacs-state-modes 'realgud-short-key-mode)
 (evil-set-initial-state 'realgud-short-key-mode 'emacs)
 (evil-set-initial-state 'Info-mode 'emacs)
