da2b2bb1d66907af6ff46d229605de72852c15a4
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-10-29 13:40:40 +0100

    evil nerd commenter config

--- init.el
+++ init.el
@@ -36,8 +36,22 @@
     (evil-mode 1))
 
 (use-package evil-nerd-commenter
-    :config
-    (evilnc-default-hotkeys))
+  :config
+  ;; Emacs key bindings
+  (global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+  (global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
+  (global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
+  (global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
+  (evil-leader/set-key
+    "ci" 'evilnc-comment-or-uncomment-lines
+    "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+    "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+    "cc" 'evilnc-copy-and-comment-lines
+    "cp" 'evilnc-comment-or-uncomment-paragraphs
+    "cr" 'comment-or-uncomment-region
+    "cv" 'evilnc-toggle-invert-comment-line-by-line
+    "."  'evilnc-copy-and-comment-operator
+    "\\" 'evilnc-comment-operator))
 
 (use-package evil-surround
     :config
