47f894371fd5934e872ade5b73612b59ab78c68c
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-11-06 16:58:51 +0100

    Separate file for custom set variables

---
+++ custom.el
@@ -0,0 +1,9 @@
+  (custom-set-variables
+   '(custom-safe-themes
+     (quote
+      ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+   '(initial-frame-alist (quote ((fullscreen . maximized))))
+   '(safe-local-variable-values
+     (quote
+      ((pyvenv-activate . "/Users/andrew/anaconda3/envs/genbank-qc")
+       (projectile-project-test-cmd . "~/anaconda3/envs/genbankfilter/bin/pytest -s test/test_*")))))
