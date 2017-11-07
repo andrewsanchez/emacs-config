0e652da303b2edc2fb73827bcd64bfa72ffbb9a3
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-11-07 13:44:52 +0100

    Separate file for config vars

--- init.el
+++ init.el
@@ -738,13 +738,3 @@ _k_: kill        _s_: split                   _{_: wrap with { }
   (add-to-list 'evil-emacs-state-modes 'gnugo-board-mode)
   (add-hook 'gnugo-start-game-hook 'gnugo-image-display-mode)
   (setq gnugo-xpms 'gnugo-imgen-create-xpms))
-
-(custom-set-variables
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(safe-local-variable-values
-   (quote
-    ((pyvenv-activate . "/Users/andrew/anaconda3/envs/genbank-qc")
-     (projectile-project-test-cmd . "~/anaconda3/envs/genbankfilter/bin/pytest -s test/test_*")))))
