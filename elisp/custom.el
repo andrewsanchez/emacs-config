a58860619ef9db95db4d1ff3b0474fd40fff9910
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-12-28 15:58:36 -0700

    merge custom files

--- elisp/custom.el
+++ elisp/custom.el
@@ -6,7 +6,36 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(org-src-window-setup (quote current-window)))
+ '(fill-column 80)
+ '(global-visual-line-mode t)
+ '(indent-tabs-mode nil)
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(org-agenda-files
+   (quote
+    ("~/org/bookmarks.org" "/Users/andrew/org/agenda/PMI.org" "/Users/andrew/org/agenda/gtd.org" "/Users/andrew/org/projects/life.org" "/Users/andrew/org/projects/memworth.org")))
+ '(org-footnote-section "References")
+ '(org-html-postamble nil)
+ '(org-src-window-setup (quote current-window))
+ '(org-startup-folded nil)
+ '(org-use-property-inheritance t)
+ '(package-selected-packages
+   (quote
+    (slurm-mode helpful link-hint avy visual-fill-column helm-projectile ob-python gnugo realgud neotree restart-emacs keyfreq elfeed-org elfeed deft flycheck linum-relative google-translate helm-wordnet company solarized-theme projectile exec-path-from-shell yapfify which-key magit zoom-frm golden-ratio org-plus-contrib yasnippet use-package smart-mode-line org-pomodoro ob-ipython hydra helm-descbinds expand-region evil-surround evil-nerd-commenter evil-matchit evil-lisp-state evil-leader)))
+ '(rst-compile-toolsets
+   (quote
+    ((html "rst2html.py" ".html" nil)
+     (latex "rst2latex" ".tex" nil)
+     (newlatex "rst2newlatex" ".tex" nil)
+     (pseudoxml "rst2pseudoxml" ".xml" nil)
+     (xml "rst2xml" ".xml" nil)
+     (pdf "rst2pdf" ".pdf" nil)
+     (s5 "rst2s5" ".html" nil))))
+ '(safe-local-variable-values
+   (quote
+    ((pyvenv-activate . "/Users/andrew/anaconda3/envs/genbank-qc")
+     (projectile-project-test-cmd . "~/anaconda3/envs/genbankfilter/bin/pytest -s test/test_*"))))
+ '(truncate-lines t)
+ '(vc-follow-symlinks nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
